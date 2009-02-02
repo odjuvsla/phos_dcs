99a2777baf3742e89885a43fcc1354ead0701505
Oystein Djuvsland <odjuvsla@pr-djuvsland.cern.ch>
2009-02-02 15:29:04 +0100

    updating new gui

--- src/phos_gui/testgui.py
+++ src/phos_gui/testgui.py
@@ -7,29 +7,8 @@ from PyQt4 import QtCore, QtGui, uic
 app = QtGui.QApplication(sys.argv)
 window_class, base_class = uic.loadUiType("phos_gui.ui")
 
-class testGui(QtGui.QMainWindow, window_class):
-
-    def __init__(self, *args):
-        QtGui.QMainWindow.__init__(self, *args)
-        self.setupUi(self)
         
-    def initFee(self):
-
-        tmpQrect = copy.deepcopy(self.fee_1_a.geometry())
-
-        self.pushButton_fee_a = range(14)
-        self.pushButton_fee_a[0] = self.fee_1_a
-        
-        for i in range(1,14):
-
-            self.pushButton_fee_a[i] = QtGui.QPushButton(self.frame_rcu0)
-            tmpQrect.setX(tmpQrect.x()+11)
-            tmpQrect.setWidth(16)
-            self.pushButton_fee_a[i].setGeometry(tmpQrect)
-
-            
-window = testGui()
-window.initFee()
+window = phosGui()
 window.show()
 app.exec_()
 
